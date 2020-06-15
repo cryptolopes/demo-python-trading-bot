e8604f33601caa0842a25e58c13bc5dfd7fee46e
Gilles <gilles.champollion@gmail.com>
2020-06-15 17:44:29 +0200

    Update SimpleBot.py

--- SimpleBot.py
+++ SimpleBot.py
@@ -13,7 +13,7 @@ class SimpleBot:
         self.log_file = "bot_" + s + ".log" 
         self.node = "https://nodes.wavesnodes.com"
         self.chain = "mainnet"
-        self.matcher = "https://matcher.waves.exchange"
+        self.matcher = "https://matcher.waves.exchange" 
         self.order_fee = int(0.003 * 10 ** 8)
         self.order_lifetime = 29 * 86400  # 29 days
         self.private_key = ""
